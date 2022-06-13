2e019e0323cc4136772d6c3e6eaae2a7c100712e
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-06-14 00:39:29 +0100

    Fix main.py

--- main.py
+++ main.py
@@ -22,84 +22,93 @@ options = ["R", "P", "S"]
 user_choice = None
 computer_choice = None
 
-def new_game():
-    print(instructions)
-    start()
-
 # shows computer choice
 def show_computer_choice():
     computer_choice = random.choice(options)
     # show the user what the computer chose
     if computer_choice == "R":
         print("Computer chose Rock!")
+        return computer_choice
     elif computer_choice == "P":
         print("Computer chose Paper!")
+        return computer_choice
     else:
         print("Computer chose Scissors!")
+        return computer_choice
 
-# this function 
+def new_game():
+    print(instructions)
+    start()
+
+# this function starts the game
 def start():
+    # this runs when the user wins a game        
+    def win():
+        print("You Win!!!")
+        print("Congratulations, You won this round")
+        to_play_again = input("do you want to play again (y/n): ")
+        if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
+            start()
+        elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
+            print("The game ended in a win")
+    # this runs when a user losses a game
+    def loss():
+        print("Computer Wins!!!")
+        print("sorry, you lost this round")
+        to_play_again = input("do you want to play again (y/n): ")
+        if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
+            start()
+        elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
+            print("The game ended in a loss")
+        
+    # this validates who wins, looses or if there is a draw
+    def validate():
+        print("validating")
+        print(user_choice, computer_choice)
+        # if the user's choice is the same as the computer's choice
+        if user_choice == "R" and computer_choice == "R" or user_choice == "P" and computer_choice == "P" or user_choice == "S" and computer_choice == "S":
+            print("Draw!!!")
+            to_start_again = input("do you want to start again (y/n): ")
+            if to_start_again == "y" or to_start_again == "yes":
+                start()
+            else:
+                print("The game ended in a draw")
+        # if the user chooses Rock and the computer chooses Paper
+        elif user_choice == "R" and computer_choice == "P":
+            loss()    
+        # if the user chooses Rock and computer chooses Scissors
+        elif user_choice == "R" and computer_choice == "S":
+            win()
+        # if the user chooses Scissors and computer chooses Paper
+        elif user_choice == "S" and computer_choice == "P":
+            win()
+        # if the user chooses Scissors and computer chooses Rock
+        elif user_choice == "S" and computer_choice == "R":
+            loss()
+        # if the user chooses Paper and computer chooses Rock
+        elif user_choice == "P" and computer_choice == "R":
+            win()
+        # if the user chooses Paper and computer chooses Scissors
+        elif user_choice == "P" and computer_choice == "S":
+            loss()
+            
+    # this gets the user's choice'
     user_choice = input("pick an option between \"R\", \"P\" or \"S\": ").upper()
     # show the user what they chose
     if user_choice == "R":
         print("You chose Rock!")
-        show_computer_choice()
+        computer_choice = show_computer_choice()
         validate()
     elif user_choice == "P":
         print("You chose Paper!")
-        show_computer_choice()
+        computer_choice = show_computer_choice()
         validate()
     elif user_choice == "S":
         print("You chose Scissors!")
-        show_computer_choice()
+        computer_choice = show_computer_choice()
         validate()
     else:
         print("invalid choice, you are to chose an option between \"R\", \"P\" or \"S\".")
-# this validates who wins, looses or if there is a draw
-def validate():
-    # if the user's choice is the same as the computer's choice
-    if user_choice == "R" and computer_choice == "R" or user_choice == "P" and computer_choice == "P" or user_choice == "S" and computer_choice == "S":
-        print("Draw!!!")
-        to_start_again = input("do you want to start again (y/n): ")
-        if to_start_again == "y" or to_start_again == "yes":
-            start()
-        else:
-            print("The game ended in a draw")
-    # if the user chooses Rock and the computer chooses Paper
-    elif user_choice == "R" and computer_choice == "P":
-        loss()    
-    # if the user chooses Rock and computer chooses Scissors
-    elif user_choice == "R" and computer_choice == "S":
-        win()
-    # if the user chooses Scissors and computer chooses Paper
-    elif user_choice == "S" and computer_choice == "P":
-        win()
-    # if the user chooses Scissors and computer chooses Rock
-    elif user_choice == "S" and computer_choice == "R":
-        loss()
-    # if the user chooses Paper and computer chooses Rock
-    elif user_choice == "P" and computer_choice == "R":
-        win()
-    # if the user chooses Paper and computer chooses Scissors
-    elif user_choice == "P" and computer_choice == "S":
-        loss()
-# this runs when the user wins a game        
-def win():
-    print("You Win!!!")
-    print("Congratulations, You won this round")
-    to_play_again = input("do you want to play again (y/n): ")
-    if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
-        start()
-    elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
-        print("The game ended in a win")
-# this runs when a user losses a game
-def loss():
-    print("Computer Wins!!!")
-    print("sorry, you lost this round")
-    to_play_again = input("do you want to play again (y/n): ")
-    if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
-        start()
-    elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
-        print("The game ended in a loss")
 
+# this starts the game
 new_game()
