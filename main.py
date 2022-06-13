1e419e5bea8d031132a79054afab11207314f180
coded1guy <grggmmnl@gmail.com>
2022-06-13 20:16:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 instructions = """
 ------------------------------------
 Here is the instruction for the game
@@ -16,13 +18,87 @@ then the winner is chosen.
 Have fun with it :)
 -------------------
 """
-def newGame():
+
+def new_game():
     print(instructions)
     start()
 
 options = ["R", "P", "S"]
+user_choice = None
+computer_choice = None
+# shows computer choice
+def show_computer_choice():
+    computer_choice = random.choice(options)
+    # show the user what the computer chose
+    if computer_choice == "R":
+        print("Computer chose Rock!")
+    elif computer_choice == "P":
+        print("Computer chose Paper!")
+    else:
+        print("Computer chose Scissors!")
 
 def start():
-    userChoice = input("pick an option between \"R\", \"P\" or \"S\": ")
+    user_choice = input("pick an option between \"R\", \"P\" or \"S\": ").upper()
+    # show the user what they chose
+    if user_choice == "R":
+        print("You chose Rock!")
+        show_computer_choice()
+        validate()
+    elif user_choice == "P":
+        print("You chose Paper!")
+        show_computer_choice()
+        validate()
+    elif user_choice == "S":
+        print("You chose Scissors!")
+        show_computer_choice()
+        validate()
+    else:
+        print("invalid choice, you are to chose an option between \"R\", \"P\" or \"S\".")
 
-newGame()
+def validate():
+    # if the user's choice is the same as the computer's choice
+    if user_choice == "R" and computer_choice == "R" or user_choice == "P" and computer_choice == "P" or user_choice == "S" and computer_choice == "S":
+        print("Draw!!!")
+        to_start_again = input("do you want to start again (y/n): ")
+        if to_start_again == "y" or to_start_again == "yes":
+            start()
+        else:
+            print("The game ended in a draw")
+    # if the user chooses Rock and the computer chooses Paper
+    elif user_choice == "R" and computer_choice == "P":
+        loss()    
+    # if the user chooses Rock and computer chooses Scissors
+    elif user_choice == "R" and computer_choice == "S":
+        win()
+    # if the user chooses Scissors and computer chooses Paper
+    elif user_choice == "S" and computer_choice == "P":
+        win()
+    # if the user chooses Scissors and computer chooses Rock
+    elif user_choice == "S" and computer_choice == "R":
+        loss()
+    # if the user chooses Paper and computer chooses Rock
+    elif user_choice == "P" and computer_choice == "R":
+        win()
+    # if the user chooses Paper and computer chooses Scissors
+    elif user_choice == "P" and computer_choice == "S":
+        loss()
+# this runs when the user wins a game        
+def win():
+    print("You Win!!!")
+    print("Congratulations, You won this round")
+    to_play_again = input("do you want to play again (y/n): ")
+    if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
+        start()
+    elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
+        print("The game ended in a win")
+
+def loss():
+    print("Computer Wins!!!")
+    print("sorry, you lost this round")
+    to_play_again = input("do you want to play again (y/n): ")
+    if to_play_again.lower() == "y" or to_play_again.lower() == "yes":
+        start()
+    elif to_play_again.lower() == "n" or to_play_again.lower() == "no":
+        print("The game ended in a loss")
+
+new_game()
